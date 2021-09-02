b1022a61664d8d510c7ef78745f3e1ad7e8054ec
Ritesh Kumar shukla <riteshshukla2381@gmail.com>
2021-09-02 10:22:59 +0530

    First

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv');
+dotenv.config();
+const nodemailer = require("nodemailer");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const app = express()
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+var cors = require('cors')
+app.use(cors())
+app.use(bodyParser.json())
+
+
+
+app.post('/', async (req, res) => {
+    const {email,message,subject,name} = req.body;
+
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, 
+        auth: {
+            user: process.env.EMAIL, 
+            pass: process.env.PASSWORD, 
+        },
+    });
+    
+    const msg = {
+        from:  ` ${name}`,
+        to: process.env.EMAIL2,
+        subject: `${subject}`, 
+        text: `Email->${email}\n${message}`
+    }
+    const info = await transporter.sendMail(msg).catch(error=>console.log(error));
+    res.send('Email Sent!')
 })
 
-app.listen(process.env.PORT||3000)
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
